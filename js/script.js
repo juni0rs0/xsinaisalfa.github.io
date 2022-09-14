fcdc62266bce8f316ba218bb42d0f579b5e4b43c
Marcos Antônio <marcos.devx1@gmail.com>
2022-09-13 22:10:13 -0300

    salavip-perguntas

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ link.classList.add("ativo")
 }
 
 links.forEach(ativarLink);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntasvip button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
