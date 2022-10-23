7d844aa887a122fc489e3856c19d464303051dbb
Marcos Antônio <marcos.devx1@gmail.com>
2022-10-23 09:52:08 -0300

    23/10

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ link.classList.add("ativo")
 }
 
 links.forEach(ativarLink);
-
 // Perguntas Frequentes
 const perguntas = document.querySelectorAll(".perguntas button");
 
@@ -29,4 +28,45 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+
+  if(!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
+    element.setAttribute(outside, '');
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      })
+      callback();
+    }
+  }
+}
+
+//
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
+  
+  if(menuButton) {
+  function openMenu(event) {
+    menuList.classList.add('active');
+    menuButton.classList.add('active');
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active');
+      menuButton.classList.remove('active');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
+}
 
